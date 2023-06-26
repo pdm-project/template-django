af4b9980a598a675968b8c6071697ec7b318fd50
Frost Ming <me@frostming.com>
2023-06-26 09:21:39 +0800

    change settings path
    Signed-off-by: Frost Ming <me@frostming.com>

--- django_app/asgi.py
+++ django_app/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for myapp project.
+ASGI config for django_app project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "myapp.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "django_app.settings")
 
 application = get_asgi_application()
--- django_app/wsgi.py
+++ django_app/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for myapp project.
+WSGI config for django_app project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "myapp.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "django_app.settings")
 
 application = get_wsgi_application()
